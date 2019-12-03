bd5adaf7ccf0e05a17e5348bcfd237d39579c871
Brandon Matthews <bmatthews@linkedin.com>
2019-12-03 10:03:03 -0800

    Complete doc comments for day 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
+/// AoC Day 1
 pub mod day1 {
     pub use error::*;
     use std::fs::File;
     use std::io::{BufRead, BufReader};
 
+    /// Error conditions related to day1
     mod error {
         #[derive(Debug)]
+        /// Day 1-related error conditions
         pub enum Error {
-            File,
+            /// Error reading or opening file
             IoError(std::io::Error),
+            /// Unable to parse int
             ParseIntError(std::num::ParseIntError),
         }
 
@@ -30,15 +34,21 @@ pub mod day1 {
         }
     }
 
+    /// Generate output for day 1, both parts
     pub fn run() -> Result<String, Error> {
         let total_fuel = total_fuel("input/day1.txt")?;
 
         Ok(format!("{}", total_fuel))
     }
 
-    /// The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually
-    /// calculate the fuel needed for the mass of each module (your puzzle input), then add
-    /// together all the fuel values.
+    /// The Fuel Counter-Upper needs to know the total fuel requirement.
+    ///
+    /// To find it, individually calculate the fuel needed for the mass of each module (your puzzle
+    /// input), then add together all the fuel values.
+    /// ...
+    /// So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel
+    /// amount you just calculated as the input mass and repeat the process, continuing until a
+    /// fuel requirement is zero or negative.
     ///
     /// What is the sum of the fuel requirements for all of the modules on your spacecraft?
     pub fn total_fuel(input_filename: &str) -> Result<u64, Error> {
@@ -52,6 +62,13 @@ pub mod day1 {
             .sum())
     }
 
+    /// Rocket-Equation compensator
+    ///
+    /// Fuel itself requires fuel just like a module - take its mass, divide by three, round down,
+    /// and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so
+    /// on. Any mass that would require negative fuel should instead be treated as if it requires
+    /// zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has
+    /// no mass and is outside the scope of this calculation.
     pub fn tyrannical_fcu(mass: u64) -> u64 {
         let mut sum = 0;
         let mut d = fuel_counter_upper(mass);
